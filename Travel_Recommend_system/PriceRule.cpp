cebad154cbc627c4b96811e13732f45ad6fec504
wdk <u202012059@hust.edu.cn>
2022-04-21 06:36:17 -0700

    Interface function(return_information)

--- Travel_Recommend_system/PriceRule.cpp
+++ Travel_Recommend_system/PriceRule.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "PriceRule.h"
+//获取信息
+vector<string> PriceRule::Return_agency() {
+    return agency;
+}
+
+string PriceRule::Return_sCity() {
+    return sCity;
+}
+
+string PriceRule::Return_dCity() {
+    return dCity;
+}
+
+string PriceRule::Return_carrier() {
+    return carrier;
+}
+
+string PriceRule::Return_nextCarrier() {
+    return nextCarrier;
+}
+
+int PriceRule::Return_sequenceNo() {
+    return sequenceNo;
+}
+
+char PriceRule::Return_surcharge() {
+    return surcharge;
+}
