ecea2bec1a9e9424b83645bd27374239112ade1a
ycr <1360605054@qq.com>
2022-05-29 05:35:27 -0700

    change the remainingSeatTable and create a new thread

--- Travel_Recommend_system/RemainSeatTable.cpp
+++ Travel_Recommend_system/RemainSeatTable.cpp
@@ -10,14 +10,25 @@ void RemainSeatTable::CreatRemainSeatTable(string path) {
         cout<<"cannot open the file"<<endl;
         return;
     }
-    Time tmp;
     while(getline(in,str)){
         vector<string> data = mysplit(str,",");
-        tmp.string2time(data[4]);
+        string tmp = data[4].substr(0,8);
         map<string,RemainingSeat> table;
-        seatTable.insert(pair<Time,map<string,RemainingSeat>>(tmp,table));
+        seatTable.insert(pair<string,map<string,RemainingSeat>>(tmp,table));
         RemainingSeat st(data);
         seatTable.at(tmp).insert(pair<string,RemainingSeat>(data[1],st));
     }
     in.close();
+}
+
+void RemainSeatTable::update() {
+    sleep(10);
+    map<string,map<string ,RemainingSeat>>::iterator iter1;
+    for(iter1 = seatTable.begin();iter1!=seatTable.end();iter1++){
+        map<string ,RemainingSeat>::iterator iter2;
+        for(iter2 = (iter1->second).begin();iter2!=(iter1->second).end();iter2++){
+            (iter2->second).updateSeats();
+        }
+    }
+    return;
 }
