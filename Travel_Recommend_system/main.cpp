e08620ff4e6d8b32676058bcd0f6dca97bdc48f9
IcathianRain <2205794866@qq.com>
2022-06-15 10:03:59 +0800

    服务器修改

--- Travel_Recommend_system/main.cpp
+++ Travel_Recommend_system/main.cpp
@@ -24,11 +24,15 @@ PriceTable *PT;
 RemainSeatTable *RST;
 
 int initialize();
+
+void srv_setup();
+
 int main() {
 
     if(initialize()==-1){
         return -1;
     }
+    // Test
 //    Time t[4];
 //    t[0].string2time("20220621000000");
 //    t[1].string2time("20220622000000");
@@ -57,38 +61,7 @@ int main() {
 //    PRT->findAgency("JZ","WDF","FEG");
 //    PRT->findAgency("CA","WDF","FEG");
 
-    httplib::Server svr;
-    Json::Reader read;
-    svr.Post("/query", [&](const httplib::Request &req, httplib::Response &res) {
-        string req_data = req.body;
-        Json::Value value;
-        read.parse(req_data, value);
-        char passNum = value.get("N", 0).asInt();
-        Json::Value rawAgc = value.get("agency", 0);
-        vector<string> agc;
-        for(int i = 0; i<rawAgc.size(); i++)
-        {
-            agc.push_back(rawAgc[i].asString());
-        }
-        int flightNum = value.get("M", 0).asInt();
-        Json::Value rawDate = value.get("date", 0);
-        Json::Value rawsCity = value.get("sCity", 0);
-        Json::Value rawdCity = value.get("dCity", 0);
-        vector<FlightRequest> flightReq;
-        for(int i = 0; i<flightNum; i++)
-        {
-            Time t;
-            t.string2time(rawDate[i].asString());
-            FlightRequest a(t, rawsCity[i].asString(), rawdCity[i].asString(), agc, passNum, 20);
-            flightReq.push_back(a);
-        }
-        vector<FlightAns> ans = SET->request(flightReq);
-        cout<<"ANs number:"<<ans.size()<<endl;
-        for(int i = 0; i < ans.size(); i++) ans[i].ShowAns();
-        res.set_content(req.body, "text/plain");
-    });
-
-    svr.listen("0.0.0.0", 8080);
+    srv_setup("0.0.0.0", 8080);
 
 
     return 0;
@@ -141,4 +114,107 @@ int initialize(){
     cout<<"gather finishing!"<<endl<<endl;
 
     cout << "Down" << endl;
+}
+
+/**
+ * @brief http服务器启动
+ * 
+ * @param ip_addr 监听的ip地址
+ * @param port 端口
+ */
+
+void srv_setup(string ip_addr, int port)
+{
+        // 初始化svr
+        httplib::Server svr;
+        // read:读取json
+        Json::Reader read;
+        svr.Post("/query", [&](const httplib::Request &req, httplib::Response &res)
+        {
+            // 获取request的数据
+            string req_data = req.body;
+            Json::Value value;
+            read.parse(req_data, value);
+            // 获取乘客人数
+            char passNum = value.get("N", 0).asInt();
+            // 获取agc列表
+            Json::Value rawAgc = value.get("agency", 0);
+            vector<string> agc;
+            for(int i = 0; i<rawAgc.size(); i++)
+            {
+                agc.push_back(rawAgc[i].asString());
+            }
+            // 分段接收每一段的数据
+            int flightNum = value.get("M", 0).asInt();
+            Json::Value rawDate = value.get("date", 0);
+            Json::Value rawsCity = value.get("sCity", 0);
+            Json::Value rawdCity = value.get("dCity", 0);
+            vector<FlightRequest> flightReq;
+            for(int i = 0; i<flightNum; i++)
+            {
+                Time t;
+                t.string2time(rawDate[i].asString());
+                FlightRequest a(t, rawsCity[i].asString(), rawdCity[i].asString(), agc, passNum, 20);
+                flightReq.push_back(a);
+            }
+            // 获取结果
+            vector<FlightAns> ans = SET->request(flightReq);
+            Json::Value res_value;
+            Json::Value res_data;
+            Json::Value res_meta;
+            res_value["meta"] = res_meta;
+            res_value["data"] = res_data;
+
+            // cout<<"ANs number:"<<ans.size()<<endl;
+            // for(int i = 0; i < ans.size(); i++) ans[i].ShowAns();
+            // 将结果封装
+            if(ans.size() > 0)
+            {
+                res_meta["msg"] = "获取成功"
+                res_meta["status"] = 200;
+                res_data["ansNum"] = ans.size();
+                for(int i = 0; i<ans.size(); i++)
+                {
+                    Json::Value res_ans;
+                    res_ans["ticketPrice"] = ans[i].Return_ticketPrice();
+                    vector<string> res_agc = ans[i].Return_agc();
+                    vector<AnsElement> res_flight = ans[i].Return_flight();
+                    for(int j = 0; i<res_agc.size(); j++)
+                    {
+                        res_ans["agc"][j] = res_agc[j];
+                    }
+                    for(int j = 0; j<res_flight.size(); j++)
+                    {
+                        Json::Value res_flight_ans;
+                        res_flight_ans["flightNo"] = res_flight[j].Return_flightNo();
+                        res_flight_ans["agc"] = res_flight[j].Return_agc();
+                        res_flight_ans["takeOfffTime"] = res_flight[j].Return_takeOffTime().time2string();
+                        res_flight_ans["arriveTime"] = res_flight[j].Return_arriveTime().time2string();
+                        res_flight_ans["sCity"] = res_flight[j].Return_sCity();
+                        res_flight_ans["dCity"] = res_flight[j].Return_dCity();
+                        res_flight_ans["seatF"] = res_flight[j].Return_seatF();
+                        res_flight_ans["seatC"] = res_flight[j].Return_seatC();
+                        res_flight_ans["seatY"] = res_flight[j].Return_seatY();
+                        char *rawSeat = res_flight[j].Return_passenger_seatList();
+                        string passengerSeatList;
+                        for(int k = 0; k<8 && rawSeat[k] != 0; k++)
+                        {
+                            passengerSeatList += rawSeat[k];
+                        }
+                        res_flight_ans["passengetSeatList"] = passengerSeatList;
+                        res_flight_ans["price"] = res_flight[j].Return_price();
+                        res_ans["flight"][j] = res_flight_ans;
+                    }
+                    res_data["ans"][i] = res_ans;
+                }
+            }
+            else
+            {
+                res_meta["msg"] = "未获取到符合条件的航班"
+                res_meta["status"] = 400;
+            }
+            res.set_content(req.body, "text/plain"); 
+        });
+
+        svr.listen(ip_addr.c_str(), port);
 }
